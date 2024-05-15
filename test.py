5295939cd366eab9f7154b844922c852d5bd27ba
moonhyeseong07 <moonhyeseong2007@gmail.com>
2024-05-15 22:44:22 +0900

    opencv

--- test.py
+++ test.py
@@ -1,38 +1,35 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+img1=cv2.imread("img11.jpg",cv2.IMREAD_GRAYSCALE)
 
-def apply_filter(frame, kernel_size):
-    kernel = np.ones((kernel_size, kernel_size), np.float32) / (kernel_size * kernel_size)
-    filtered_frame = cv2.filter2D(frame, -1, kernel)
-    return filtered_frame
+h,w, = img1.shape
+tlans_x=10; tlans_y = 25
+point1_src=np.float32([[15,20],[50,70],[130,140]])
+point1_dst=np.float32(point1_src+[tlans_x,tlans_y])
+affine_mat1=cv2.getAffineTransform(point1_src,point1_dst)
+user_mat1=np.float32([[1,0,tlans_x],[0,1,tlans_y]])
+res1=cv2.warpAffine(img1,affine_mat1,(w,h))
+res2=cv2.warpAffine(img1,user_mat1,(w,h))
 
-def on_trackbar_change(val):
-    global filter_size, frame
-    filter_size = val
-    filtered_frame = apply_filter(frame, filter_size)
-    cv2.imshow('Filtered Frame', filtered_frame)
+scale_x=0.8; scale_y=0.6
+user_mat2=np.float32([[scale_x,0,0],[0,scale_y,0]])
+res3=cv2.warpAffine(img1,user_mat2,(w,h))
+res4=cv2.resize(img1,(0,0),None,scale_x,scale_y)
+background=np.full(shape=[h,w],fill_value=0,dtype=np.uint8)
+background[:round(h*scale_y),:round(w*scale_x)]=res4;
+res4=background
+user_mat3=np.float32([[0.4,0,100],[0,0.6,50]])
+res5 = cv2.warpAffine(img1,user_mat3,(w,h))
+ress=[]
+ress.append(img1),ress.append(res1),ress.append(res2)
+ress.append(res3),ress.append(res4),ress.append(res5)
+titles=['input','res1','res2','res3','res4','res5']
 
-CAMERA_ID = 0
-cam = cv2.VideoCapture(CAMERA_ID)
-if not cam.isOpened():
-    print('Cannot open the camera-%d' % (CAMERA_ID))
-    exit()
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(ress[i],cmap='gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
 
-cv2.namedWindow('CAM Window')
-
-filter_size = 1  
-ret, frame = cam.read()  
-
-
-cv2.createTrackbar('Filter Size', 'CAM Window', filter_size, 20, on_trackbar_change)
-
-while True:
-    cv2.imshow('CAM Window',frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-
-    ret,frame = cam.read() 
-
-cam.release()
-cv2.destroyAllWindows()
+plt.show()
