49270e2ba2d383f0f47da2c714bae39d8312d134
moonhyeseong07 <moonhyeseong2007@gmail.com>
2024-05-20 22:56:52 +0900

    5/20

--- test.py
+++ test.py
@@ -9,13 +9,14 @@ point1_src=np.float32([[15,20],[50,70],[130,140]])
 point1_dst=np.float32(point1_src+[tlans_x,tlans_y])
 affine_mat1=cv2.getAffineTransform(point1_src,point1_dst)
 user_mat1=np.float32([[1,0,tlans_x],[0,1,tlans_y]])
+
 res1=cv2.warpAffine(img1,affine_mat1,(w,h))
 res2=cv2.warpAffine(img1,user_mat1,(w,h))
-
 scale_x=0.8; scale_y=0.6
 user_mat2=np.float32([[scale_x,0,0],[0,scale_y,0]])
 res3=cv2.warpAffine(img1,user_mat2,(w,h))
 res4=cv2.resize(img1,(0,0),None,scale_x,scale_y)
+
 background=np.full(shape=[h,w],fill_value=0,dtype=np.uint8)
 background[:round(h*scale_y),:round(w*scale_x)]=res4;
 res4=background
@@ -33,4 +34,4 @@ for i in range(6):
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
-plt.show()
+plt.show() 
